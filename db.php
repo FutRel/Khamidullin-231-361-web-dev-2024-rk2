fa1278ab46eb09d033fe95b7e8551c9524c374fd
FutREl <gxrim@bk.ru>
2024-11-01 17:16:21 +0300

    db db

--- db.php
+++ db.php
@@ -4,10 +4,10 @@ $db = 'web';
 $user = 'root';
 $pass = 'SQL1234';
 
-/*try {
+try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Ошибка подключения: " . $e->getMessage());
-}*/
+}
 
